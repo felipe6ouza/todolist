a11892a2634523a684341699709b0526376013b7
Felipe Souza <felipe6ouza@gmail.com>
2024-10-07 21:14:34 -0300

    feat - Remove código não utilizado para salvar histórico tarefa do DbContext

--- src/Todolist.Infrastructure/Context/TodolistDbContext.cs
+++ src/Todolist.Infrastructure/Context/TodolistDbContext.cs
@@ -1,10 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.ChangeTracking;
-using System.Text.Json;
 using Todolist.Domain.Aggregates;
 using Todolist.Domain.Entities;
 using Todolist.Domain.Shared;
-using Todolist.Domain.ValueObjects;
 using Todolist.Domain.View;
 using Todolist.Infrastructure.Mappings;
 using Tarefa = Todolist.Domain.Aggregates.Tarefa;
@@ -45,69 +42,7 @@ namespace Todolist.Infrastructure.Context
 
         public async Task<bool> Commit()
         {
-           if(ChangeTracker.Entries<Tarefa>().Any(e => e.State == EntityState.Modified))
-           {
-               await GerarHistoricoAsync();
-           }
             return await SaveChangesAsync() > 1;
         }
-
-        private async Task GerarHistoricoAsync()
-        {
-            var historicos = new List<HistoricoTarefa>();
-
-            foreach (var entry in ChangeTracker.Entries<Tarefa>()
-                         .Where(e => e.State == EntityState.Modified))
-            {
-                var modificacoes = CapturarModificacoes(entry);
-
-
-                var modificacoesJson = JsonSerializer.Serialize(modificacoes);
-
-                if (modificacoes.Any())
-                {
-                    var historico = new HistoricoTarefa(
-                        entry.Entity.Id,
-                        modificacoesJson,
-                        DateTime.UtcNow,
-                        GetCurrentUserId()
-                    );
-
-                    historicos.Add(historico);
-                }
-            }
-
-            if (historicos.Count != 0)
-            {
-                await HistoricoTarefas.AddRangeAsync(historicos);
-            }
-        }
-
-        private static List<Modificacao> CapturarModificacoes(EntityEntry<Tarefa> entry)
-        {
-            var modificacoes = new List<Modificacao>();
-
-            foreach (var prop in entry.Properties.Where(p => p.IsModified))
-            {
-                var valorAnterior = prop.OriginalValue?.ToString();
-                var valorNovo = prop.CurrentValue?.ToString();
-
-                if (valorAnterior != valorNovo)
-                {
-                    modificacoes.Add(new Modificacao(prop.Metadata.Name, valorAnterior, valorNovo));
-                }
-            }
-
-            return modificacoes;
-        }
-
-        private int GetCurrentUserId()
-        {
-            // Lógica para obter o ID do usuário autenticado
-            return 1; // Exemplo
-        }
-
-
-
     }
 }
